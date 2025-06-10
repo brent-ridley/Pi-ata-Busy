7455d7a00d016d692929565ebf7b74f9c9c272e6
brent-ridley <brent.ridley@icloud.com>
2025-06-09 20:38:47 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>Welcome to Piñata Bust</h1>
+      <p>This is the working React frontend setup.</p>
+    </div>
+  );
+}
+
+export default App;
